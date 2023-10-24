34af9b41a69f247c7b47c4b7842ef9ef20ef711e
martin2236 <martoxxx100@gmail.com>
2023-10-23 22:44:29 -0300

    contentful

--- public/js/poductosTabs.js
+++ public/js/poductosTabs.js
@@ -1,4 +1,21 @@
-(function main() {
+const organizarDatos = (items, assets) =>{
+    let itemsOrganizados = items.map( (item) => {
+        let imagen = assets.find((asset) => asset.sys.id === item.fields.foto.sys.id);
+        return {...item,imagen}
+    })
+    return itemsOrganizados; 
+ } 
+
+const getInfo = async() =>{
+    let res = await fetch('https://cdn.contentful.com/spaces/dq87mr462mv8/environments/master/entries?access_token=SKuUJoKsvmG_zuRkicdIBzIsnsBShV2677CDCe6go3A')
+    let data = await res.json();
+    let assets = data.includes.Asset
+    let items =  data.items
+    const itemsOrganizados = organizarDatos(items, assets);
+    return itemsOrganizados;
+  }
+
+(async function main() {
     document.addEventListener("DOMContentLoaded", function () {
       // Obtener el parámetro 'tab' de la URL
       const urlParams = new URLSearchParams(window.location.search);
@@ -50,5 +67,66 @@
         link.classList.add("current"); // Agregar el estilo activo al botón de la pestaña seleccionada
       });
     });
+
+   const items = await getInfo();
+   const tattoos = items.filter(item => item.fields.tipo == 'tattoo');
+   const diseños = items.filter(item => item.fields.tipo == 'diseño');
+   const cuadros = items.filter(item => item.fields.tipo == 'cuadro');
+
+   const tattoosDiv = document.getElementById('tattooRow');
+
+        const tattooDivs = tattoos.map(tattoo => {
+        const div = document.createElement('div');
+        div.classList.add('col-lg-4', 'col-md-6', 'mt-4');
+        div.innerHTML = `
+            <img src="${tattoo.imagen.fields.file.url}" alt="${tattoo.imagen.fields.title}" class="img-fluid">
+        `;
+        return div;
+        });
+
+        tattoosDiv.innerHTML = '';
+
+        tattooDivs.forEach(tattooDiv => {
+        tattoosDiv.appendChild(tattooDiv);
+        });
+            //DISEÑOS
+
+        const diseñosDiv = document.getElementById('designsRow');
+
+        const diseñoDivs = diseños.map(diseño => {
+        const div = document.createElement('div');
+        div.classList.add('col-lg-4', 'col-md-6', 'mt-4');
+        div.innerHTML = `
+            <img src="${diseño.imagen.fields.file.url}" alt="${diseño.imagen.fields.title}" class="img-fluid">
+        `;
+        return div;
+        });
+
+        diseñosDiv.innerHTML = '';
+
+        diseñoDivs.forEach(diseñoDiv => {
+        diseñosDiv.appendChild(diseñoDiv);
+        });
+
+        //CUADROS
+
+        const cuadrosDiv = document.getElementById('paintingsRow');
+
+        const cuadroDivs = cuadros.map(cuadro => {
+        const div = document.createElement('div');
+        div.classList.add('col-lg-4', 'col-md-6', 'mt-4');
+        div.innerHTML = `
+            <img src="${cuadro.imagen.fields.file.url}" alt="${cuadro.imagen.fields.title}" class="img-fluid">
+        `;
+        return div;
+        });
+
+        cuadrosDiv.innerHTML = '';
+
+        cuadroDivs.forEach(cuadroDiv => {
+        cuadrosDiv.appendChild(cuadroDiv);
+        });
+
   })();
   
+ 
